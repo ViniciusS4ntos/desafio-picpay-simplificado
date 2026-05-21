1f3892074b43914c3b55a32aa099d6de303a9702
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-05-20 22:22:27 -0300

    feat: create user

---
+++ src/main/java/com/vinicius/PicPaySimplificado/infras/repositorys/UserRepository.java
@@ -0,0 +1,13 @@
+package com.vinicius.PicPaySimplificado.infras.repositorys;
+
+import com.vinicius.PicPaySimplificado.infras.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Boolean existsByEmail(String email);
+    Boolean existsByCpf(String email);
+
+}
