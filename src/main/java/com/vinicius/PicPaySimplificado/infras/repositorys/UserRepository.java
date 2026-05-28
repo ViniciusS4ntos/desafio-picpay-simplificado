28ec336f46ced4a1b735240b317e35539edbf882
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-05-28 00:10:52 -0300

    fix: changes in user

--- src/main/java/com/vinicius/PicPaySimplificado/infras/repositorys/UserRepository.java
+++ src/main/java/com/vinicius/PicPaySimplificado/infras/repositorys/UserRepository.java
@@ -4,10 +4,13 @@ import com.vinicius.PicPaySimplificado.infras.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Boolean existsByEmail(String email);
     Boolean existsByCpf(String email);
+    User findByCpf(String cpf);
 
 }
