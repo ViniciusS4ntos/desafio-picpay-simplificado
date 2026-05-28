3e4f1868f2b1b4b126a96454aaae272a4511744e
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-05-28 00:13:48 -0300

    feat: show list of transactions & add money in controller

--- src/main/java/com/vinicius/PicPaySimplificado/Controller/UserController.java
+++ src/main/java/com/vinicius/PicPaySimplificado/Controller/UserController.java
@@ -2,15 +2,16 @@ package com.vinicius.PicPaySimplificado.Controller;
 
 import com.vinicius.PicPaySimplificado.Controller.dtos.in.UserDTOIn;
 import com.vinicius.PicPaySimplificado.Controller.dtos.out.UserDtoOut;
+import com.vinicius.PicPaySimplificado.infras.entities.Transaction;
 import com.vinicius.PicPaySimplificado.infras.entities.User;
 import com.vinicius.PicPaySimplificado.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -26,6 +27,17 @@ public class UserController {
     public ResponseEntity<UserDtoOut> criarUsuario(@RequestBody UserDTOIn user){
         User entity= userService.criarUsuario(mapper.map(user, User.class));
         return ResponseEntity.ok(mapper.map(entity, UserDtoOut.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> mostrarEnvios(@RequestParam("id") Integer id){
+        return ResponseEntity.ok().body(userService.listarTransacoes(id));
+    }
+
+    @PostMapping("/dinheiro")
+    public ResponseEntity<User> botarDinheiro(@RequestParam("balance")BigDecimal bigDecimal,
+                                              @RequestParam("id") Integer id){
+        return ResponseEntity.ok(userService.botarDinheiro(bigDecimal, id));
     }
 
 }
